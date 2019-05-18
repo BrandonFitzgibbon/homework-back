116085718d9a7eda0df5121220c4ba8bb5bb5cbe
Brandon Fitzgibbon <brandon_fitzgibbon@live.ca>
2019-05-18 17:39:33 -0400

    added health check

--- src/operations.ts
+++ src/operations.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express'
 
 export const operations = {
+    health: function(req: Request, res: Response) {
+        res.sendStatus(200)
+    },
     postEntry: function(req: Request, res: Response) {
         res.send(req.body)
     }
