fae237b298e27d5c70ea620a584e4f4d127c7a17
Brandon Fitzgibbon <brandon_fitzgibbon@live.ca>
2019-05-19 15:10:33 -0400

    added coverage

--- test/api.ts
+++ test/api.ts
@@ -2,11 +2,24 @@ import chai from 'chai'
 import chaiHttp from 'chai-http'
 import app from '../src/app'
 import entries from './requests/entries'
+import health from './requests/health'
 
 chai.should()
 chai.use(chaiHttp)
 
-describe('api request', () => {
+describe('api health request', () => {
+    health.forEach(request => {
+        it(request.description, () => {
+            return chai.request(app)[request.method](request.path)
+            .send()
+            .then(result => {
+                result.status.should.eql(200)
+            })
+        })
+    })
+})
+
+describe('api entry requests', () => {
     entries.forEach(entry => {
         it(entry.description, () => {
             return chai.request(app)[entry.method](entry.path)
